e73ebcd1b1dd0dea0b1a2bacc3f05fbc9e42a669
copandrej <ac8560@student.uni-lj.si>
2022-05-14 14:16:12 +0200

    attempt at adding integral

--- physics.py
+++ physics.py
@@ -1,36 +1,51 @@
-import numpy
+import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
 import glob
 
-#path for test data 
-path = os.getcwd() +"/testData"
+# functions
+def integrate(x, y):
+    area = np.trapz(y=y, x=x)
+    return area
+
+# path for test data
+path = os.getcwd() + "/testData"
 
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
 # loop over the list of csv files
 for f in csv_files:
 
-  #pandanize csv data and print
-  data = pd.read_csv(f)
-  #print(data)
+    # pandanize csv data and print
+    data = pd.read_csv(f)
+    # print(data)
 
-  #plot data
-  #data.plot(x='Time (s)')
-  #plt.show()
+    # set initial values (TODO - adjust when live data)
+    hitrost = 0
+    diffTime = 0
+    X = 0
+    par = 1
 
-  #set initial values (TODO - adjust when live data)
-  hitrost = 0
-  diffTime = 0
-  diffX = 0
+    for i in range(round(len(data)/par)):
 
-  for i in range(len(data)):
-    diffTime = data.loc[i, "Time (s)"] - diffTime
-    diffX = data.loc[i, "Linear Acceleration x (m/s^2)"]
-    hitrost +=  diffTime * diffX
-    data.loc[i,"hitrost"] = hitrost
+        if par != 1: #taking bigger sample size for inetgrate
+            df = data.loc[(i*par):(i*par+par)]  #data frame 
+            #print(df)
+            spremembaHitrosti = integrate(
+                df["Time (s)"].values, df["Linear Acceleration x (m/s^2)"].values)  # "integrate"
+            hitrost += spremembaHitrosti
 
-  data.plot(x='Time (s)')
-  plt.show()
+        else: #take every sample for "integration"
+            diffTime = data.loc[i, "Time (s)"] - diffTime
+            X = data.loc[i, "Linear Acceleration x (m/s^2)"]
+
+            # treshold
+            if(abs(X) < 1):
+                X = 0
+            hitrost += (diffTime * X)
+            data.loc[i, "hitrost"] = hitrost
+
+    data.plot(x='Time (s)')
+    plt.show()
