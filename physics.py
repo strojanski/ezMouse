0436b6859e21eba91ca215726619e349699a7d8a
copandrej <ac8560@student.uni-lj.si>
2022-05-14 14:32:46 +0200

    pot

--- physics.py
+++ physics.py
@@ -6,9 +6,12 @@ import os
 import glob
 
 # functions
+
+
 def integrate(x, y):
     area = np.trapz(y=y, x=x)
     return area
+
 
 # path for test data
 path = os.getcwd() + "/testData"
@@ -30,22 +33,24 @@ for f in csv_files:
 
     for i in range(round(len(data)/par)):
 
-        if par != 1: #taking bigger sample size for inetgrate
-            df = data.loc[(i*par):(i*par+par)]  #data frame 
-            #print(df)
+        if par != 1:  # taking bigger sample size for inetgrate
+            df = data.loc[(i*par):(i*par+par)]  # data frame
+            # print(df)
             spremembaHitrosti = integrate(
                 df["Time (s)"].values, df["Linear Acceleration x (m/s^2)"].values)  # "integrate"
             hitrost += spremembaHitrosti
 
-        else: #take every sample for "integration"
+        else:  # take every sample for "integration"
             diffTime = data.loc[i, "Time (s)"] - diffTime
             X = data.loc[i, "Linear Acceleration x (m/s^2)"]
 
             # treshold
             if(abs(X) < 1):
                 X = 0
-            hitrost += (diffTime * X)
+            hitrost += diffTime * X
+            pot += hitrost*diffTime
             data.loc[i, "hitrost"] = hitrost
+            data.loc[i, "pot"] = pot
 
     data.plot(x='Time (s)')
     plt.show()
