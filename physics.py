ddde3d1268f81bfaa35c0de4dea76bd2e75bf4df
copandrej <ac8560@student.uni-lj.si>
2022-05-14 16:56:40 +0200

    added parameters for fine tunning

--- physics.py
+++ physics.py
@@ -6,8 +6,6 @@ import os
 import glob
 
 # functions
-
-
 def integrate(x, y):
     area = np.trapz(y=y, x=x)
     return area
@@ -17,7 +15,7 @@ def smooth(y, box_pts):
     box = np.ones(box_pts)/box_pts
     y_smooth = np.convolve(y, box, mode='same')
     return y_smooth
-    
+
 # path for test data
 path = os.getcwd() + "/Testdata"
 
@@ -34,31 +32,35 @@ for f in csv_files:
     speedX = speedY = timeDiff = distanceX = distanceY = accX = accY = threshX = threshY = 0
     par = 1
 
+    # parameters (TODO adjust this parameters for best results)
+    smoothening = 15  # shows how agresive is smoothening
+    thresh = 1  # treshold for acceleration (possible values between 0 and 2)
+    threshMovment = 5  # How many times over the tresh before starting to mesure
+    stall = 10  # How many times under the tresh before velocity is set to 0
+
     # filtering signal
-    data["accX"] = smooth(data['accX'], 15)
-    data["accY"] = smooth(data['accY'], 15)
-
-
+    data["accX"] = smooth(data['accX'], smoothening)
+    data["accY"] = smooth(data['accY'], smoothening)
     # for i in range(0, (len(data) - par), par):
     for i in range(len(data)):
 
         timeDiff = data.loc[i, "time"] - timeDiff
 
-        #X axis
+        # X axis
         accX = data.loc[i, "accX"]
 
         # threshold for data cleanup. Recognize big changes in accel, and start messuring speed
-        if(abs(accX) > 1 and threshX < 20):
+        if(abs(accX) > thresh and threshX < 20):
             threshX += 1
         elif(threshX > 0):
             threshX -= 1
 
         # if a bit of time no accel change and before begining of movement set accel to 0
-        if(threshX < 5):
+        if(threshX < threshMovment):
             accX = 0
 
         # if long time no data change, then mouse is not moving and data is corupt, set speed to 0
-        if(threshX < 10):
+        if(threshX < stall):
             speedX = 0
 
         speedX += (timeDiff * accX)/100
@@ -66,22 +68,21 @@ for f in csv_files:
         data.loc[i, "speedX"] = speedX
         data.loc[i, "distanceX"] = distanceX
 
-
-        #Y axis
+        # Y axis
         accY = data.loc[i, "accY"]
 
         # threshold for data cleanup. Recognize big changes in accel, and start messuring speed
-        if(abs(accY) > 1 and threshY < 20):
+        if(abs(accY) > thresh and threshY < 20):
             threshY += 1
         elif(threshY > 0):
             threshY -= 1
 
         # if a bit of time no accel change and before begining of movement set accel to 0
-        if(threshY < 5):
+        if(threshY < threshMovment):
             accY = 0
 
         # if long time no data change, then mouse is not moving and data is corupt, set speed to 0
-        if(threshY < 10):
+        if(threshY < stall):
             speedY = 0
 
         speedY += (timeDiff * accY)/100
@@ -89,17 +90,7 @@ for f in csv_files:
         data.loc[i, "speedY"] = speedY
         data.loc[i, "distanceY"] = distanceY
 
-    data.plot(x='time', y=['accX', 'speedX', 'distanceX', 'accY', 'speedY', 'distanceY'])
+    data.plot(x='time', y=['accX', 'speedX',
+              'distanceX', 'accY', 'speedY', 'distanceY'])
     plt.title(f.removeprefix(path+"/"))
     plt.show()
-
-""" useless posibly
-if par != 1:  # taking bigger sample size for inetgrate
-    df = data.loc[(i):(i+par)]  # data frame
-    spremembaspeedi = integrate(
-        df["time"].values, df["accX"].values)  # "integrate"
-    speed += spremembaspeedi
-    data.loc[i:i+par, "speed"] = speed
-
-else:  # take every sample for "integration
-"""
