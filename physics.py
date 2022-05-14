08bfa08cbff2970a8acfd3a4e0c6bc61d00109ef
copandrej <ac8560@student.uni-lj.si>
2022-05-14 12:45:44 +0200

    data print

--- physics.py
+++ physics.py
@@ -1,5 +1,18 @@
 import numpy
 import scipy
-import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import glob
 
-print("test")
+#path for test data 
+path = os.getcwd() +"/testData"
+
+csv_files = glob.glob(os.path.join(path, "*.csv"))
+
+# loop over the list of csv files
+for f in csv_files:
+
+  #pandanize csv data and print
+  data = pd.read_csv(f)
+  print(data)
