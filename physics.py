6f2f43a94e557b485b56322e09780194624ea3c9
copandrej <ac8560@student.uni-lj.si>
2022-05-14 16:16:15 +0200

    var cleanup and threshold setting

--- physics.py
+++ physics.py
@@ -6,54 +6,66 @@ import os
 import glob
 
 # functions
+
+
 def integrate(x, y):
     area = np.trapz(y=y, x=x)
     return area
 
+
 # path for test data
-path = os.getcwd() + "/testData"
+path = os.getcwd() + "/Testdata"
 
 csv_files = glob.glob(os.path.join(path, "*.csv"))
 
 # loop over the list of csv files
 for f in csv_files:
 
-    # pandanize csv data and print
+    # pandanize csv data and renaming columns TODO adjust when live data
     data = pd.read_csv(f)
-    data.columns = ['time', 'x', 'y', 'z', 'abs']
-
-    # print(data)
+    data.columns = ['time', 'accX', 'y', 'z', 'abs']
 
     # set initial values (TODO - adjust when live data)
-    hitrost = 0
-    diffTime = 0
-    pot = 0
-    X = 0
+    speedX = speedY = timeDiff = distanceX = distanceY = accX = accY = thresh = 0
     par = 1
 
-    for i in range(0, (len(data) - par), par):
+    # for i in range(0, (len(data) - par), par):
+    for i in range(len(data)):
 
-        if par != 1: #taking bigger sample size for inetgrate
-            df = data.loc[(i):(i+par)]  #data frame 
-            #print(df)
-            spremembaHitrosti = integrate(
-                df["time"].values, df["x"].values)  # "integrate"
-            hitrost += spremembaHitrosti
-            data.loc[i:i+par, "hitrost"] = hitrost
-            #print(round((i + i + par)/2))
+        timeDiff = data.loc[i, "time"] - timeDiff
+        accX = data.loc[i, "accX"]
 
-        else:  # take every sample for "integration"
-            diffTime = data.loc[i, "time"] - diffTime
-            X = data.loc[i, "x"]
+        # threshold for data cleanup. Recognize big changes in accel, and start messuring speed
+        if(abs(accX) > 1 and thresh < 20):
+            thresh += 1
+        elif(thresh > 0):
+            thresh -= 1
 
-            # treshold
-            if(abs(X) < 0.2):
-                X = 0
-            hitrost += diffTime * X
-            pot += hitrost*diffTime
-            data.loc[i, "hitrost"] = hitrost
-            data.loc[i, "pot"] = pot
+        # if a bit of time no accel change and before begining of movement set accel to 0
+        if(thresh < 5):
+            accX = 0
 
-    data.plot(x='time', y=['x', 'hitrost'])
+        # if long time no data change, then mouse is not moving and data is corupt, set speed to 0
+        if(thresh < 10):
+            speedX = 0
+
+        speedX += (timeDiff * accX)/100
+        distanceX += (speedX*timeDiff)/100
+        data.loc[i, "speedX"] = speedX
+        data.loc[i, "distanceX"] = distanceX
+
+    data.plot(x='time', y=['accX', 'speedX', 'distanceX'])
     plt.title(f.removeprefix(path+"/"))
     plt.show()
+
+
+""" useless posibly
+if par != 1:  # taking bigger sample size for inetgrate
+    df = data.loc[(i):(i+par)]  # data frame
+    spremembaspeedi = integrate(
+        df["time"].values, df["accX"].values)  # "integrate"
+    speed += spremembaspeedi
+    data.loc[i:i+par, "speed"] = speed
+
+else:  # take every sample for "integration
+"""
